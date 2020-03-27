7cbb72cf15cd9e8da6e5014fed9fce64ad079f14
Matt Granmoe <mattgranmoe@gmail.com>
2020-03-26 21:47:32 -0500

    Stream location to component, poll every few seconds

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safe_trace/location.dart';
 
 void main() => runApp(MyApp());
 
@@ -91,13 +92,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            GetLocation(),
           ],
         ),
       ),
